7acea9b1cae675fa1b80474419c8b5ee0620f861
ImranSasudin <imransasudin@gmail.com>
2020-10-25 17:17:10 +0800

    Update progress migration

--- database/migrations/2020_10_24_132813_create_progress_table.php
+++ database/migrations/2020_10_24_132813_create_progress_table.php
@@ -14,14 +14,14 @@ class CreateProgressTable extends Migration
     public function up()
     {
         Schema::create('progress', function (Blueprint $table) {
-            $table->unsignedBigInteger('checlist_id');
+            $table->unsignedBigInteger('checklist_id');
             $table->unsignedBigInteger('project_id');
             $table->unsignedBigInteger('updated_by')->nullable();
             $table->string('status');
             $table->timestamps();
 
-            $table->primary(['checlist_id', 'project_id']);
-            $table->foreign('checlist_id')->references('id')->on('checklists')->onDelete('cascade');
+            $table->primary(['checklist_id', 'project_id']);
+            $table->foreign('checklist_id')->references('id')->on('checklists')->onDelete('cascade');
             $table->foreign('project_id')->references('id')->on('projects')->onDelete('cascade');
             $table->foreign('updated_by')->references('id')->on('users')->onDelete('set null');
         });
