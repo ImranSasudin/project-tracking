d688b14b2010ac03097eb822f60a8e60f67a9486
ImranSasudin <imransasudin@gmail.com>
2020-11-07 18:07:46 +0800

    CRUD Client Controller

---
+++ app/Http/Controllers/Admin/ClientController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Hash;
+
+class ClientController extends Controller
+{
+    public function dashboard()
+    {
+        return view('client.dashboard');
+    }
+
+    public function index()
+    {
+        $clients = User::where('role', 'client')->get();
+
+        return view('admin.client.index', [
+            'clients' => $clients,
+        ]);
+    }
+
+    public function create()
+    {
+        return view('admin.client.create');
+    }
+
+    public function store(Request $request)
+    {
+        $client = new User();
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->password = Hash::make($request->password);
+        $client->phone = $request->phone;
+        $client->address = $request->address;
+        $client->role = 'client';
+        $client->company = $request->company;
+        $client->save();
+
+        return redirect()->route('admin.client')->withSuccess('New client successfully created');
+    }
+
+    public function edit($id)
+    {
+        $client = User::find($id);
+
+        return view('admin.client.edit', [
+            'client' => $client,
+        ]);
+    }
+
+    public function update(Request $request)
+    {
+        $client = User::find($request->id);
+        $client->name = $request->name;
+        $client->email = $request->email;
+        $client->password = Hash::make($request->password);
+        $client->phone = $request->phone;
+        $client->address = $request->address;
+        $client->role = 'client';
+        $client->company = $request->company;
+        $client->save();
+
+        return redirect()->route('admin.client')->withSuccess('Client succesfully updated');
+    }
+
+    public function delete($id)
+    {
+        $client = User::destroy($id);
+
+        return redirect()->route('admin.client')->withSuccess('Client succesfully deleted');
+    }
+}
