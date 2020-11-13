9eec847957465347dbe37fc10d58700bf8370ddd
ImranSasudin <imransasudin@gmail.com>
2020-11-13 08:20:24 +0800

    Edit project blade

---
+++ resources/views/admin/project/edit.blade.php
@@ -0,0 +1,87 @@
+@extends('admin.layout.nav')
+@section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Client</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">User Management</a></li>
+                    <li class="breadcrumb-item active">Client</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6">
+                <!-- general form elements -->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Update Client</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form role="form" action="{{ route('admin.client.update') }}" method="POST">
+                        @csrf
+                        <input type="hidden" name="id" value="{{ $client->id }}">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Name</label>
+                                        <input type="text" name="name" class="form-control" placeholder="Enter name" value="{{ $client->name }}" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Email address</label>
+                                        <input type="email" name="email" class="form-control" placeholder="Enter email" value="{{ $client->email }}" required>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Phone</label>
+                                        <input type="number" name="phone" class="form-control" placeholder="Enter phone number" value="{{ $client->phone }}" required>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group">
+                                        <label>Company</label>
+                                        <input type="text" name="company" class="form-control" placeholder="Enter company name" value="{{ $client->company }}"  required>
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                        <label>Address</label>
+                                        <textarea class="form-control" name="address" placeholder="Enter address" required>{{ $client->address }}</textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <a href="javascript: history.go(-1)" class="btn btn-info">Back</a>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+@endsection
+@section('navbar')
+$("#project").addClass("active");
+@endsection
