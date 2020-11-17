b1ccfb237949738d59cada4a7382817ea4a29035
ImranSasudin <imransasudin@gmail.com>
2020-11-17 19:27:28 +0800

    create page for task

---
+++ resources/views/admin/task/create.blade.php
@@ -0,0 +1,83 @@
+@extends('admin.layout.nav')
+@section('content')
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>Task</h1>
+            </div>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item active">Task</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
+
+<!-- Main content -->
+<section class="content">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-sm-5">
+                <!-- general form elements -->
+                <div class="card card-primary">
+                    <div class="card-header">
+                        <h3 class="card-title">Add New Task</h3>
+                    </div>
+                    <!-- /.card-header -->
+                    <!-- form start -->
+                    <form role="form" action="{{ route('admin.task.store') }}" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <div class="form-group">
+                                <label>Project</label>
+                                <select name="project" class="form-control select2" required>
+                                    <option value="" disabled selected>-- Please Select --</option>
+                                    @foreach($projects as $project)
+                                    <option value="{{ $project->id }}">{{ $project->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Task Type</label>
+                                <select name="task" class="form-control select2" required>
+                                    <option value="" disabled selected>-- Please Select --</option>
+                                    @foreach($tasks as $task)
+                                    <option value="{{ $task->id }}">{{ $task->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Staff</label>
+                                <select name="staff" class="form-control select2" required>
+                                    <option value="" disabled selected>-- Please Select --</option>
+                                    @foreach($staffs as $staff)
+                                    <option value="{{ $staff->id }}">{{ $staff->name }}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label>Description</label>
+                                <textarea class="form-control" name="description" placeholder="Enter task description" required></textarea>
+                            </div>
+                        </div> <!-- /.card-body -->
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
+                    </form>
+                </div>
+                <a href="javascript: history.go(-1)" class="btn btn-info">Back</a>
+                <!-- /.card -->
+            </div>
+            <!-- /.col -->
+        </div>
+        <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+@endsection
+@section('navbar')
+$("#task").addClass("active");
+@endsection
