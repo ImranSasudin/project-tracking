360de86b1bf4c0c2fca9806fba21757148bbf2f4
ImranSasudin <imransasudin@gmail.com>
2020-11-20 07:27:14 +0800

    update dashboard

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -4,11 +4,18 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Project;
+use App\Models\Task;
 
 class AdminController extends Controller
 {
     public function dashboard()
     {
-        return view('admin.dashboard');
+        $user = User::count();
+
+        return view('admin.dashboard',[
+            'user' => $user,
+        ]);
     }
 }
