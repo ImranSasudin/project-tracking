fd9add8b5e9be14c306a479d5d0044a36aed588f
ImranSasudin <imransasudin@gmail.com>
2020-11-20 08:23:17 +0800

    Client edit project

--- resources/views/client/project/edit.blade.php
+++ resources/views/client/project/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('admin.layout.nav')
+@extends('client.layout.nav')
 @section('content')
 <!-- Content Header (Page header) -->
 <section class="content-header">
@@ -28,7 +28,7 @@
                     </div>
                     <!-- /.card-header -->
                     <!-- form start -->
-                    <form role="form" action="{{ route('admin.project.update') }}" method="POST">
+                    <form role="form" action="{{ route('client.project.update') }}" method="POST">
                         @csrf
                         <input type="hidden" name="id" value="{{ $project->id }}">
                         <div class="card-body">
@@ -44,14 +44,6 @@
                                 <label>Date</label>
                                 <input type="date" name="date" min="{{ date("Y-m-d") }}" value="{{ $project->date }}" class="form-control" placeholder="Enter date" required>
                             </div>
-                            <div class="form-group">
-                                <label for="exampleInputPassword1">Client</label>
-                                <select name="client" class="form-control select2" required>
-                                    @foreach($clients as $client)
-                                    <option value="{{ $client->id }}" {{ $client->id == $project->client_id ? 'selected' : '' }}>{{ $client->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
                         </div>
                         <!-- /.card-body -->
                         <div class="card-footer">
@@ -62,53 +54,6 @@
                 <a href="javascript: history.go(-1)" class="btn btn-info">Back</a>
                 <!-- /.card -->
             </div>
-            <!-- /.col -->
-            <div class="col-sm-7">
-                <!-- general form elements -->
-                <div class="card card-primary">
-                    <div class="card-header">
-                        <h3 class="card-title">Update Project</h3>
-                    </div>
-                    <!-- /.card-header -->
-                    <!-- form start -->
-                    <form role="form" action="{{ route('admin.project.update.progress') }}" method="POST">
-                        @csrf
-                        <input type="hidden" name="id" value="{{ $project->id }}">
-                        <div class="card-body">
-                            <table class="table table-bordered">
-                                <thead>
-                                    <tr>
-                                        <th>Num.</th>
-                                        <th>Description</th>
-                                        <th>Percentage (%)</th>
-                                        <th></th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach($progress as $progress)
-                                    <tr>
-                                        <td>{{ $progress->number }})</td>
-                                        <td>{{ $progress->description }}</td>
-                                        @if($progress->percentage != null)
-                                        <td>{{ $progress->percentage }}</td>
-                                        <td><input type="checkbox" id="{{ $progress->id }}" name="checklist[]" value="{{ $progress->id }}" {{ $progress->project_id != null ? 'checked' : '' }}></td>
-                                        @else
-                                        <td colspan="2" class="bg-secondary"></td>
-                                        @endif
-                                    </tr>                                   
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-primary">Submit</button>
-                        </div>
-                    </form>
-                </div>
-                <!-- /.card -->
-            </div>
-            <!-- /.col -->
         </div>
         <!-- /.row -->
     </div>
