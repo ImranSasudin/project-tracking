c7b65b47eae13ea756e972577aa22a3c2aa88a72
ImranSasudin <imransasudin@gmail.com>
2020-11-21 14:35:59 +0800

    change seed password

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,13 +17,13 @@ class UserSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['name'=>'admin', 'email'=>'admin@gmail.com', 'password' => Hash::make('admin'), 'phone'=>'123', 'address'=>'jasin',
+            ['name'=>'admin', 'email'=>'admin@gmail.com', 'password' => Hash::make('pass@admin'), 'phone'=>'123', 'address'=>'jasin',
             'company'=>null,'role'=>'admin', 'manager_id'=>null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
 
-            ['name'=>'staff', 'email'=>'staff@gmail.com', 'password' => Hash::make('staff'), 'phone'=>'123', 'address'=>'jasin',
+            ['name'=>'staff', 'email'=>'staff@gmail.com', 'password' => Hash::make('pass@staff'), 'phone'=>'123', 'address'=>'jasin',
             'company'=>null,'role'=>'staff', 'manager_id'=>'1', 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()],
 
-            ['name'=>'client', 'email'=>'client@gmail.com', 'password' => Hash::make('client'), 'phone'=>'123', 'address'=>'jasin',
+            ['name'=>'client', 'email'=>'client@gmail.com', 'password' => Hash::make('pass@client'), 'phone'=>'123', 'address'=>'jasin',
             'company'=>'ABC Company', 'role'=>'client', 'manager_id'=>null, 'created_at' => Carbon::now(), 'updated_at' => Carbon::now()], 
 
         ];
