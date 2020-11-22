c4fae72042fceb08f3334f5afcfbfc12f92c7777
ImranSasudin <imransasudin@gmail.com>
2020-11-22 12:14:46 +0800

    update client dashboard

--- app/Http/Controllers/Client/ClientController.php
+++ app/Http/Controllers/Client/ClientController.php
@@ -4,11 +4,16 @@ namespace App\Http\Controllers\Client;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ClientController extends Controller
 {
     public function dashboard()
     {
-        return view('client.dashboard');
+        $project = Project::count();
+
+        return view('client.dashboard', [
+            'project' => $project,
+        ]);
     }
 }
