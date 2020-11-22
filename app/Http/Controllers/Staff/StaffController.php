0b1dc5def49461492e77c0f950c40087ead555f1
ImranSasudin <imransasudin@gmail.com>
2020-11-22 12:14:34 +0800

    update staff dashboard

--- app/Http/Controllers/Staff/StaffController.php
+++ app/Http/Controllers/Staff/StaffController.php
@@ -5,11 +5,20 @@ namespace App\Http\Controllers\Staff;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Project;
 
 class StaffController extends Controller
 {
     public function dashboard()
     {
-        return view('staff.dashboard');
+        $staff = User::whereIn('role',['staff','admin'])->count();
+        $client = User::where('role','client')->count();
+        $project = Project::count();
+
+        return view('staff.dashboard', [
+            'staff' => $staff,
+            'client' => $client,
+            'project' => $project,
+        ]);
     }
 }
